b13674c1460819eb6df357ab3ba5a2963f206301
Frcru <91342407+Frcru@users.noreply.github.com>
2024-03-10 02:05:16 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,50 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function excuse() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuseGenerator();
+  // $("#excuse").text(excuseGenerator());
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function excuseGenerator() {
+  //Generate all index
+  let excuseGenerated;
+  let whoIndex = indexGenerator(who);
+  let actionIndex = indexGenerator(action);
+  let whatIndex = indexGenerator(what);
+  let whenIndex = indexGenerator(when);
+
+  //Array to get all random array's index to
+  let allIndex = [whoIndex, actionIndex, whatIndex, whenIndex];
+
+  excuseGenerated = mergeString(allIndex);
+
+  return excuseGenerated;
+}
+
+function indexGenerator(size) {
+  let index = Math.floor(Math.random() * size.length);
+
+  return index;
+}
+
+function mergeString(allIndex) {
+  let stringGenerated;
+
+  stringGenerated = `${who[allIndex[0]]} 
+      ${action[allIndex[1]]} 
+      ${what[allIndex[2]]} 
+      ${when[allIndex[3]]}`;
+  return stringGenerated;
+}
